8b8a96dfb9de235b332111f6496bedcf589aa5cf
diego sacramento <diegoferrarez@gmail.com>
2025-09-09 19:30:45 -0300

    first commit

---
+++ src/test/java/br/com/scto/revenue/RevenueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.scto.revenue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevenueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
